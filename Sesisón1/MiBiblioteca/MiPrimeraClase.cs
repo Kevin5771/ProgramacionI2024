b4dadce9dbe3e48e850b27d89fc0cf159eea7b60
Kevin5771 <kevin577gutierrez@gmail.com>
2024-02-13 19:48:23 -0600

    First commit

---
+++ Sesisón1/MiBiblioteca/MiPrimeraClase.cs
@@ -0,0 +1,13 @@
+﻿namespace MiBiblioteca
+{
+    public class MiPrimeraClase
+    {
+
+        //Modificacdor de acceso: public tipo de retorno: decimal NombreDelMetodo:sumar
+        public decimal Sumar(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+
+        }
+    }
+}
---
+++ Sesisón1/Sesisón1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+//Sirve para agregar distintas classes C#
+
+//Importamos la biblioteca de clases 
+using MiBiblioteca;
+
+namespace Sesión1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de Variables
+            //Cada Lina de código en C# lleva ;
+            int edadNumero = 0;
+
+            MiPrimeraClase miPrimerObjeto = new MiPrimeraClase();
+            decimal suma = miPrimerObjeto.Sumar(10, 20);
+            Console.WriteLine("El resultado de la Suma es {0}", suma);
+
+            /// Para compilar usamos dotnet build 
+            //Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad: ");
+            string edad = Console.ReadLine();
+            edadNumero = int.Parse(edad);
+
+
+            if (edadNumero < 18)
+            {
+                Console.WriteLine("Usted es menor de Edad");
+
+            } else
+            {
+                Console.WriteLine("Usted es Una Persona Mayor de edad");
+            }
+
+
+            Console.ReadLine();
+        }
+
+
+        }
+            
+    
+}
+  
+
+
