c6162927bc735cf91a9cfbaa07444b6d762183c3
Kevin5771 <kevin577gutierrez@gmail.com>
2024-02-17 10:48:20 -0600

    Semana 3 Trabajo en Clases

---
+++ Sesion_2/Sesion_2/Pizarra.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion_2
+{
+    public class Pizarra
+    {
+        //Atributos del objeto
+        public string color;
+        private double ancho;
+        public double largo;
+
+        public string contenido = "";
+
+
+        public Pizarra (string color, double ancho, double largo)
+        {
+            this.color = color;
+            this.ancho = ancho;
+            this.largo = largo;
+        }
+
+        /// <summary>
+        /// El método escribe el contenido indicado
+        /// </summary>
+        /// <param name="texto"></param>
+        public void Escribir(string texto)
+        {
+            
+            contenido = contenido + texto;
+
+        }
+        /// <summary>
+        /// Método que borrara el contenido de la pizarra
+        /// </summary>
+        public void Borrar() {
+            contenido = ""; }
+
+         public String GetContenido () {
+            return contenido;
+        }
+
+    }
+}
---
+++ Sesion_2/Sesion_2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Sesion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Ingresa tu nombre!");
+            //String nombre = Console.ReadLine();
+            // Console.WriteLine("Hello " + nombre);
+
+            Pizarra Pizarron = new Pizarra("Blanco", 1.5, 3.1);
+            Pizarron.Escribir("Hola estudiantes");
+
+            Console.WriteLine(Pizarron.GetContenido());
+
+        }
+    }
+}
