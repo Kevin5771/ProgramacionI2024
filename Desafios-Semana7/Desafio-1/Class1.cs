4ffc05379db7d9e577405aa3274300546bae2924
Kevin5771 <kevin577gutierrez@gmail.com>
2024-03-20 21:05:47 -0600

    Tarea: Desafios Semana 7

---
+++ Desafios-Semana7/Desafio-1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    public class Class1
+    {
+
+        public char[,] tablero;
+
+        public Class1() {
+            tablero = new char[3, 3];
+            Iniciar_tablero();
+        }
+
+        private void Iniciar_tablero()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    tablero[i, j] = ' ';
+                }
+            }
+        }
+
+        public void Dibujar_Tablero() {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(tablero[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+                Console.WriteLine();
+                if (i < 2) {
+                    Console.WriteLine("----------");
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ Desafios-Semana7/Desafio-1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!!!");
+            Class1 totito = new Class1();
+            totito.Dibujar_Tablero();
+
+        }
+    }
+}
