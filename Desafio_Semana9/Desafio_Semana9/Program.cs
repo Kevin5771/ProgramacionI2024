7b2a72d5c2702c5274f51988c35e957bc9b980c3
Kevin5771 <kevin577gutierrez@gmail.com>
2024-04-12 19:59:11 -0600

    Desafio Semana 9

---
+++ Desafio_Semana9/Desafio_Semana9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Desafio_Semana9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!!!");
+
+            string texto = "Este es mi Correo Electrónico: kgutierrezl2@miumg.edu.gt;  Por favor, envíeme un correo con sus comentarios.";
+
+            // Expresión regular para encontrar direcciones de correo electrónico válidas
+            string patronCorreo = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b";
+
+            // El objeto Regex busca el patrón de correo electrónico
+            Regex busquedaCorreo = new Regex(patronCorreo);
+
+            MatchCollection coincidencias = busquedaCorreo.Matches(texto);
+
+            // Direcciones de correo electrónico encontradas
+            Console.WriteLine("Direcciones de correo electrónico encontradas:");
+            foreach (Match match in coincidencias)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
