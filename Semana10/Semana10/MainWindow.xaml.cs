370bc0b4fcaebfd4a69cd4aed57b0a9bf0a02a24
Kevin5771 <kevin577gutierrez@gmail.com>
2024-05-16 19:16:47 -0600

    Desafio semana 10

---
+++ Semana10/Semana10/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semana10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Grid grid = new Grid();
+            //this.Content = grid;
+            //Button btn = new Button();
+            //btn.FontSize = 12;
+            //WrapPanel wrapPanel = new WrapPanel();
+            //TextBlock textBlock = new TextBlock();
+            //textBlock.Text = "Botton";
+            //textBlock.Foreground = Brushes.Blue;
+
+            //wrapPanel.Children.Add(textBlock);
+
+            //btn.Content = wrapPanel;
+            //grid.Children.Add(btn);
+
+            
+        }
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {       
+            Button button = (Button)sender;
+            MessageBox.Show("Has Presionado el Boton");
+        }
+    }
+}
